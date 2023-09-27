71470d1ae8ff291af5c5bf9baf834596b3b32142
Cristobal Cepeda <cceplag@gmail.com>
2023-09-27 14:03:52 +0900

    Add get_spread_info file

---
+++ wages_division/get_spread_info.py
@@ -0,0 +1,4 @@
+## First we get the data from the master spreadsheet
+## Second we filter the important data
+## Third we loop through the hostess names and update each file and spreadsheet
+## Fourth we update the master spreadsheet ---- OPTIONAL
