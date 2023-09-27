a5afea0c48121da92a6ce7bcaff6d209d439b9ab
Cristobal Cepeda <cceplag@gmail.com>
2023-09-27 16:31:43 +0900

    add get_hostess_dict to generate a dict with name and sheet_id

--- wages_division/get_spread_info.py
+++ wages_division/get_spread_info.py
@@ -2,3 +2,27 @@
 ## Second we filter the important data
 ## Third we loop through the hostess names and update each file and spreadsheet
 ## Fourth we update the master spreadsheet ---- OPTIONAL
+import gspread
+import google.auth
+
+def get_hostess_dict(master_id):
+    try:
+        try:
+            gc = gspread.service_account(filename=absolute_path)
+        except:
+            credentials, _ = google.auth.default()
+            gc = gspread.authorize(credentials)
+
+
+        nyc_master_hostess_data = gc.open_by_key(master_id)
+        worksheet_name = "MASTER"
+        worksheet = nyc_master_hostess_data.worksheet(worksheet_name)
+
+        a_col = worksheet.get_values("A2:A")
+        n_col = worksheet.get_values("N2:N")
+
+        hostes_dict = {A[0]:N[0] for A,N in zip(a_col,n_col)}
+    except Exception as e:
+        print(e)
+        return False
+    return hostes_dict
