4973cb50f16bf477fa0bc6f0c0c5cb105d51f27f
Cristobal Cepeda <cceplag@gmail.com>
2023-09-27 14:02:48 +0900

    add new function to process all files

--- wages_division/process.py
+++ wages_division/process.py
@@ -11,12 +11,17 @@ from time import sleep
 from query import create_query
 
 
-def get_spreadsheet(spreadsheet_name=None):
+def get_spreadsheet(spreadsheet_name=None, spreadsheet_id=None):
     # Specify the Google Sheets document and worksheet
+
     credentials, _ = google.auth.default()
     gc = gspread.authorize(credentials)
+
     # Open the Google Sheets document
-    sh = gc.open(spreadsheet_name)
+    if spreadsheet_id:
+        sh = gc.open_by_key(spreadsheet_id)
+    else:
+        sh = gc.open(spreadsheet_name)
     return sh
 
 
@@ -151,6 +156,77 @@ def format_worksheet(worksheet):
 
 
 def update_google_sheets_with_retry(results_df, sh, hostess_name):
+    def clean_cell(unit_cell):
+        if unit_cell.value.lower() in ["nan", "none", "nat", "null", "<na>", " "]:
+            unit_cell.value = ""
+        else:
+            pass
+        try:
+            unit_cell.value = float(unit_cell.value)
+        except:
+            pass
+        return unit_cell
+
+    ## In case the hostess_name is "all" we will process all the hostess
+    ## In case the hostess_name is not "all" we will process only that hostess
+
+    if hostess_name == "all":
+        list_hostess = results_df["hostess_name"].unique()
+    elif hostess_name != "all":
+        list_hostess = [hostess_name]
+    ## TODO : add the month number to the query
+    for name in list_hostess:
+        while True:
+            try:
+                df_temp = get_specific_hostess_df(results_df, name)
+
+                sheets = [sht_name.title for sht_name in sh.worksheets()]
+
+                if name not in sheets:
+                    new_sheet_name = name
+                    new_worksheet = sh.add_worksheet(
+                        title=name,
+                        rows=str(df_temp.shape[0]),
+                        cols=str(df_temp.shape[1]),
+                    )
+                else:
+                    new_worksheet = sh.worksheet(name)
+
+                new_worksheet.clear()
+                cell_list = new_worksheet.range(
+                    1, 1, len(df_temp) + 1, len(df_temp.columns)
+                )
+
+                for cell in cell_list:
+                    if cell.row == 1:
+                        # Set headers in the first row
+                        cell.value = df_temp.columns[cell.col - 1]
+                    else:
+                        # Set data from the DataFrame without single quotes
+                        cell.value = str(df_temp.iloc[cell.row - 2, cell.col - 1])
+                cell_list = [clean_cell(cell_dirty) for cell_dirty in cell_list]
+
+                new_worksheet.update_cells(cell_list, value_input_option="USER_ENTERED")
+                try:
+                    format_worksheet(new_worksheet)
+                except Exception as e:
+                    print(f"Couldn't format {name} Sheet")
+                    print(e)
+                break  # Exit the retry loop if successful
+            except Exception as e:
+                if "RATE_LIMIT_EXCEEDED" in str(e):
+                    waiting_time = 10
+                    print(
+                        f"API rate limit exceeded. Waiting {waiting_time} and retrying {name} sheet"
+                    )
+                    sleep(waiting_time)  # Wait for 10 seconds before retrying, then wait 20, then 30, etc.
+                    waiting_time = waiting_time + 10
+                else:
+                    print("Some other error ocurred")
+                    print(e)
+                    raise  # Re-raise the exception if it's not a rate limit error
+
+def update_all_sheets(results_df, sh, hostess_name):
     def clean_cell(unit_cell):
         if unit_cell.value.lower() in ["nan", "none", "nat", "null", "<na>", " "]:
             unit_cell.value = ""
