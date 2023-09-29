6f38811c8c31c3a421fc58797461de94fed36e79
Cristobal Cepeda <cceplag@gmail.com>
2023-09-29 12:35:48 +0900

    hostess_salary get_spread_info current

--- wages_division/get_spread_info.py
+++ wages_division/get_spread_info.py
@@ -1,7 +1,3 @@
-## First we get the data from the master spreadsheet
-## Second we filter the important data
-## Third we loop through the hostess names and update each file and spreadsheet
-## Fourth we update the master spreadsheet ---- OPTIONAL
 import gspread
 import google.auth
 
