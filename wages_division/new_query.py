fa04aeab29d76fd302141303fc66368ba3089f98
Cristobal Cepeda <cceplag@gmail.com>
2023-10-20 15:52:17 +0900

    env var error fix

--- wages_division/new_query.py
+++ wages_division/new_query.py
@@ -7,7 +7,7 @@ TABLE_2 = os.environ["TABLE_2"]
 TABLE_3 = os.environ["TABLE_3"]
 TABLE_4 = os.environ["TABLE_4"]
 PRODUCT_3 = os.environ["PRODUCT_3"]
-EXTRA_H = os.environ["EXTRA"]
+EXTRA_H = os.environ["EXTRA_H"]
 
 def create_new_query(month,year=2023):
     month_str = (2-len(str(month)))*"0" + str(month) ## Add a zero if the month is less than 10
