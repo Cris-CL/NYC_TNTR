271da44b7b84182f3c1ff87d4cc87e13efeb3e87
Cristobal Cepeda <cceplag@gmail.com>
2023-12-11 16:44:01 +0900

    wages part updated

--- tantra_master_updater/wages.py
+++ tantra_master_updater/wages.py
@@ -10,6 +10,7 @@ HOSTESS_WAGE = os.environ.get("HOSTESS_WAGE")
 DATASET = os.environ.get("DATASET")
 PROJECT_ID = os.environ.get("PROJECT_ID")
 
+
 def replace_wages(year_month):
 
     client = bigquery.Client()
@@ -29,39 +30,68 @@ def replace_wages(year_month):
         raise e
     return
 
-def update_wages(sheet_name):
-  if sheet_name == 'MASTER':
-    return
 
-    print(f'Sheet {HOSTESS_WAGE} with id: {WAGES_SHEET_ID} is being updated')
-
+def get_wages(sheet_name):
     credentials, _ = google.auth.default()
     gc = gspread.authorize(credentials)
-    integer_cols = {"hour_a":"int64","hour_b":"int64","cp_code":"int64"}
+    integer_cols = {"hour_a": "int64", "hour_b": "int64", "cp_code": "int64"}
     try:
-        extra_sh = gc.open_by_key(WAGES_SHEET_ID)
-        ws = extra_sh.worksheet(sheet_name)
+        wages_sh = gc.open_by_key(WAGES_SHEET_ID)
+        ws = wages_sh.worksheet(sheet_name)
 
-        df =pd.DataFrame(ws.get_all_values())
+        df = pd.DataFrame(ws.get_all_values())
         df = df.iloc[1:]
         df = df[df.columns[0:7]]
-        df.columns = ["hostess_name","real_name","wage_class","cp_code","hour_a","hour_b","YEAR_MONTH"]
+        df.columns = [
+            "hostess_name",
+            "real_name",
+            "wage_class",
+            "cp_code",
+            "hour_a",
+            "hour_b",
+            "YEAR_MONTH",
+        ]
         ### Clean the data
         for col in integer_cols.keys():
-            df[col] = df[col].map(lambda x: x.replace(",","").replace(" ","").replace("(","-").replace(")","").replace(".00","") if isinstance(x,str) else x)
-            df[col] = df[col].map(lambda x: 0 if isinstance(x,str) and x == "" else x)
-        df = df.astype({f'{col}':f'{integer_cols.get(col,"string")}' for col in df.columns})
+            df[col] = df[col].map(
+                lambda x: x.replace(",", "")
+                .replace(" ", "")
+                .replace("(", "-")
+                .replace(")", "")
+                .replace(".00", "")
+                if isinstance(x, str)
+                else x
+            )
+            df[col] = df[col].map(lambda x: 0 if isinstance(x, str) and x == "" else x)
+        df = df.astype(
+            {f"{col}": f'{integer_cols.get(col,"string")}' for col in df.columns}
+        )
         for col in df.columns:
             if col not in integer_cols.keys():
-                df[col] = df[col].map(lambda x: None if isinstance(x,str) and x == "" else x)
+                df[col] = df[col].map(
+                    lambda x: None if isinstance(x, str) and x == "" else x
+                )
 
         df["UPLOADED"] = datetime.now()
-        df = df[df['YEAR_MONTH'].notna()]
+        df = df[df["YEAR_MONTH"].notna()]
+    except Exception as e:
+        print(e)
+    return df
+
+
+def update_wages(sheet_name):
+    if sheet_name == "MASTER":
+        return
+    print(f"Sheet {HOSTESS_WAGE} with id: {WAGES_SHEET_ID} is being updated")
+
+    try:
+        df = get_wages(sheet_name)
         df.to_gbq(
-        destination_table=f"{DATASET}.{HOSTESS_WAGE}_2",
-        project_id=PROJECT_ID,
-        progress_bar=False,
-        if_exists="append")
+            destination_table=f"{DATASET}.{HOSTESS_WAGE}",
+            project_id=PROJECT_ID,
+            progress_bar=False,
+            if_exists="append",
+        )
 
         ### Take year_month from current data and keep the latest uploaded version
         year_mon = df["YEAR_MONTH"].unique()[0]
