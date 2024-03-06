55a5cec5812ad265ebb59c3c82bd22813fe30bc1
Cristobal Cepeda <cceplag@gmail.com>
2024-03-06 17:29:51 +0900

    add docstring and format

--- hostess_salary_updater/main.py
+++ hostess_salary_updater/main.py
@@ -8,33 +8,35 @@ import pytz
 
 URL = os.environ.get("URL")
 
-def send_request(month_select,test_var=True):
+
+def send_request(month_select, test_var=True):
+    """
+    This function sends a POST request to the URL with the month and year as the payload.
+    the trigger for this function is a cloud scheduler job that runs every day, 2 times,
+    one for the previous month and one for the current month.
+    """
     if test_var == True:
-        name_var = 'Test'
+        name_var = "Test"
     if test_var == False:
-        name_var = 'All'
-        current_hour = datetime.utcnow().hour+9
+        name_var = "All"
+        current_hour = datetime.utcnow().hour + 9
         print(f"Sending update request at {current_hour}")
-    timezone_name = 'Asia/Tokyo'
+    timezone_name = "Asia/Tokyo"
     dt = datetime.now(pytz.timezone(timezone_name))
-    if month_select ==1:
+    if month_select == 1:
         month = dt.month
-    elif month_select ==2:
+    elif month_select == 2:
         dt = dt - relativedelta(months=1)
         month = dt.month
     else:
         print(f"Wrong message {month_select}")
         return
     year = dt.year
-    data_send = {
-        "name": name_var,
-        "month": month,
-        "year":year
-        }
-    headers = {'Content-Type': 'application/json'}
+    data_send = {"name": name_var, "month": month, "year": year}
+    headers = {"Content-Type": "application/json"}
     print(data_send)
     try:
-      requests.post(URL,json=data_send,headers=headers,timeout=5)
+        requests.post(URL, json=data_send, headers=headers, timeout=5)
 
     # except requests.exceptions.ReadTimeout:
     #     print(f"POST request Send to {URL}")
@@ -43,13 +45,14 @@ def send_request(month_select,test_var=True):
         print(f"POST request Send to {URL}")
     return
 
+
 @functions_framework.cloud_event
 def updater(cloud_event):
-    month_sel = cloud_event.data["message"]['attributes']['month_select']
-    if 'previous' in month_sel:
-        send_request(month_select=2,test_var=False)
-    elif 'current' in month_sel:
-        send_request(month_select=1,test_var=False)
+    month_sel = cloud_event.data["message"]["attributes"]["month_select"]
+    if "previous" in month_sel:
+        send_request(month_select=2, test_var=False)
+    elif "current" in month_sel:
+        send_request(month_select=1, test_var=False)
     else:
         print("something wrong")
     print(cloud_event.data["message"])
