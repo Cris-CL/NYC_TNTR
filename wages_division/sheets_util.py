820eeb0deeb64c4bcb016b274a02dfc504012a56
Cristobal Cepeda <cceplag@gmail.com>
2024-08-01 13:14:56 +0900

    move get_hostess_dict from get_spread_info.py

--- wages_division/sheets_util.py
+++ wages_division/sheets_util.py
@@ -1,4 +1,28 @@
+import gspread
+import google.auth
 from time import sleep
+
+def get_hostess_dict(master_id):
+    try:
+        try:
+            gc = gspread.service_account()
+        except:
+            credentials, _ = google.auth.default()
+            gc = gspread.authorize(credentials)
+
+        nyc_master_hostess_data = gc.open_by_key(master_id)
+        worksheet_name = "MASTER"  #### Maybe this should be an environment variable
+        worksheet = nyc_master_hostess_data.worksheet(worksheet_name)
+
+        a_col = worksheet.get_values("A2:A")
+        p_col = worksheet.get_values("P2:P")
+
+        hostes_dict = {A[0]:P[0] for A,P in zip(a_col,p_col)}
+    except Exception as e:
+        print(e)
+        return False
+    return hostes_dict
+
 
 
 def clear_formatting(FILE, sheet_name):
