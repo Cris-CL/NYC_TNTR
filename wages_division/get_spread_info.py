57701a434f52a3065f916ff629b9520827012634
Cristobal Cepeda <cceplag@gmail.com>
2024-11-15 17:24:58 +0900

    add retry for get_hostess_dict

--- wages_division/get_spread_info.py
+++ wages_division/get_spread_info.py
@@ -1,8 +1,9 @@
 import gspread
 import google.auth
+from time import sleep
 
 
-def get_hostess_dict(master_id):
+def get_hostess_dict_0(master_id):
     try:
         try:
             gc = gspread.service_account()
@@ -19,6 +20,39 @@ def get_hostess_dict(master_id):
 
         hostes_dict = {A[0]:P[0] for A,P in zip(a_col,p_col)}
     except Exception as e:
-        print(e)
+        print("Error in get_hostess_dict",e)
         return False
+    return hostes_dict
+
+
+def get_hostess_dict(master_id):
+    sleep_time = 31
+    retry_count = 0
+    try:
+        gc = gspread.service_account()
+    except:
+        credentials, _ = google.auth.default()
+        gc = gspread.authorize(credentials)
+    while True:
+        try:
+            nyc_master_hostess_data = gc.open_by_key(master_id)
+            worksheet_name = "MASTER"  #### Maybe this should be an environment variable
+            worksheet = nyc_master_hostess_data.worksheet(worksheet_name)
+
+            a_col = worksheet.get_values("A2:A")
+            p_col = worksheet.get_values("P2:P")
+
+            hostes_dict = {A[0]:P[0] for A,P in zip(a_col,p_col)}
+            break
+        except Exception as e:
+            print('Error in get_hostess_dict',e)
+            if retry_count > 0:
+                retry_count += 1
+                print('Reached max retries for get_hostess_dict')
+                return False
+            else:
+                print("Retrying get_hostess_dict after 30 seconds wait")
+                retry_count += 1
+                sleep(sleep_time)
+                continue
     return hostes_dict
